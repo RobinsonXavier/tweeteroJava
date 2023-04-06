14c61d1f986c905a9f47a0a251a8b3cec6d36a37
RobinsonXavier <robinson.webzen@gmail.com>
2023-04-06 14:30:18 -0300

    Finished UserController

--- src/main/java/com/tweetero/api/controller/UserController.java
+++ src/main/java/com/tweetero/api/controller/UserController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweetero.api.dto.UserDTO;
+import com.tweetero.api.models.User;
+import com.tweetero.api.repository.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,10 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/sign-up")
 public class UserController {
+
+  @Autowired
+  private UserRepository repository;
   
   @PostMapping
-  public void create(@RequestBody UserDTO user) {
-      
+  public String create(@RequestBody UserDTO user) {
+    repository.save(new User(user));
+    return "OK";
   }
   
 }
--- src/main/java/com/tweetero/api/models/User.java
+++ src/main/java/com/tweetero/api/models/User.java
@@ -1,15 +1,25 @@
 package com.tweetero.api.models;
 
+import com.tweetero.api.dto.UserDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class User {
+
+  public User(UserDTO data) {
+    this.username = data.username();
+    this.avatar = data.avatar();
+  }
+
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
