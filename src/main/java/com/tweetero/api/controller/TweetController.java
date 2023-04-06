2ca9e8a7b19f817729bf73e177395dbec4b9ed6c
RobinsonXavier <robinson.webzen@gmail.com>
2023-04-06 17:24:43 -0300

    Created TweetService

--- src/main/java/com/tweetero/api/controller/TweetController.java
+++ src/main/java/com/tweetero/api/controller/TweetController.java
@@ -1,6 +1,5 @@
 package com.tweetero.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,56 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tweetero.api.dto.TweetDTO;
 import com.tweetero.api.models.Tweet;
-import com.tweetero.api.models.Account;
-import com.tweetero.api.repository.TweetRepository;
-import com.tweetero.api.repository.AccountRepository;
+import com.tweetero.api.service.TweetService;
 
 @RestController
 @RequestMapping("/tweets")
 public class TweetController {
 
   @Autowired
-  private TweetRepository repository;
-
-  @Autowired
-  private AccountRepository userRepository;
+  private TweetService service;
 
   @PostMapping
   public String create(@RequestBody TweetDTO tweet) {
-    Account user = userRepository.findByUsername(tweet.username());
-
-    repository.save(new Tweet(tweet, user.getAvatar()));
-    return "OK";
+    return service.createTweet(tweet);
   }
 
   @GetMapping
   public List<Tweet> getTweets(@RequestParam(name = "page", required = false) String page) {
-    List<Tweet> list = repository.findAll();
-    List<Tweet> newList = new ArrayList<Tweet>();
-    int size = list.size();
-
-    for (int i = size - 1; i > 0; i--) {
-      if(page != null) {
-        int multi = Integer.parseInt(page);
-        multi = multi * 5;
-
-        for(int j = multi; j > 0; j--) {
-          newList.add(list.get(i));
-          i--;
-        }
-
-        return newList;
-      }
-      
-      newList.add(list.get(i));
-    }
-
-    return newList;
+    return service.getTweets(page);
   }
 
 
   @GetMapping("/{username}")
   public List<Tweet> getTweetsByUsername(@PathVariable String username) {
-    return repository.findByUsername(username);
+    return service.getTweetsByUsername(username);
   }
 }
---
+++ src/main/java/com/tweetero/api/service/TweetService.java
@@ -0,0 +1,58 @@
+package com.tweetero.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tweetero.api.dto.TweetDTO;
+import com.tweetero.api.models.Account;
+import com.tweetero.api.models.Tweet;
+import com.tweetero.api.repository.AccountRepository;
+import com.tweetero.api.repository.TweetRepository;
+
+@Service
+public class TweetService {
+  
+  @Autowired
+  private TweetRepository repository;
+
+  @Autowired
+  private AccountRepository accountRepository;
+
+  public String createTweet(TweetDTO req) {
+    Account user = accountRepository.findByUsername(req.username());
+    repository.save(new Tweet(req, user.getAvatar()));
+
+    return "OK";
+  }
+
+  public List<Tweet> getTweets(String page) {
+    List<Tweet> list = repository.findAll();
+    List<Tweet> newList = new ArrayList<Tweet>();
+    int size = list.size();
+
+    for (int i = size - 1; i > 0; i--) {
+      if(page != null) {
+        int multi = Integer.parseInt(page);
+        multi = multi * 5;
+
+        for(int j = multi; j > 0; j--) {
+          newList.add(list.get(i));
+          i--;
+        }
+
+        return newList;
+      }
+      
+      newList.add(list.get(i));
+    }
+
+    return newList;
+  }
+
+  public List<Tweet> getTweetsByUsername(String username) {
+    return repository.findByUsername(username);
+  }
+}
