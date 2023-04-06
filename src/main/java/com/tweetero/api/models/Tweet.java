96c41262637317a5692c2b2dff26e416e24eb8b4
RobinsonXavier <robinson.webzen@gmail.com>
2023-04-06 14:37:02 -0300

    Created Tweet DTO and model

---
+++ src/main/java/com/tweetero/api/models/Tweet.java
@@ -0,0 +1,36 @@
+package com.tweetero.api.models;
+
+import com.tweetero.api.dto.TweetDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Tweet {
+
+  public Tweet(TweetDTO data, String avatar) {
+    this.username = data.username();
+    this.text = data.tweet();
+    this.avatar = avatar;
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 50, nullable = false)
+  private String username;
+
+  @Column(length = 200, nullable = false)
+  private String avatar;
+
+  @Column(length = 200, nullable = false)
+  private String text;
+}
